c8418dbad8d6afed5c7d183586cda7a2845fc84c
SyedUsamaRehman <mohammadusamaz11@gmail.com>
2026-06-13 23:01:28 +0500

    BUg find in url of oringin resolved

--- eld_backend/settings.py
+++ eld_backend/settings.py
@@ -27,7 +27,7 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
-"https://eld-app-frontend.vercel.app/"
+"https://eld-app-frontend.vercel.app"
 ]
 
 ROOT_URLCONF = 'eld_backend.urls'
